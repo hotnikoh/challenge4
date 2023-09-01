ce05377a274627e87faf3a9be4d425b6f12d7d11
Hotniko Hildebrandus <htniko93@gmail.com>
2023-09-01 21:02:53 +0700

    first commit

---
+++ fahrtocurry.js
@@ -0,0 +1,11 @@
+function celciusTofahrenheit(celcius){
+    return function() {
+        let celcToFarhn = (celcius * 9/5) + 32;
+        return celcToFarhn;
+    }  
+    
+}
+
+let celcius = 20;
+let convrtCelcFahr = celciusTofahrenheit(celcius);
+console.log(convrtCelcFahr())
