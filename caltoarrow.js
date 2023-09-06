ecd784d6b47b73251175f438bf7cbbd61249c52d
Hotniko Hildebrandus <htniko93@gmail.com>
2023-09-06 15:16:23 +0700

    commit lagi

--- caltoarrow.js
+++ caltoarrow.js
@@ -23,3 +23,31 @@ let calculator = (input1, input2, operation) => {
 
 let result = calculator (input1, input2, operation);
 console.log (result);
+
+//==================================================
+
+// let input1 = 10
+// let input2 = 5
+// let operation = "/"
+
+// let calculator = (input1, input2, operation) => {
+//     if (typeof input1 !== "number" || typeof input2 !== "number") {
+//         return "Invalid input, input must be a number";
+//     }
+
+//     if (operation =="+"){
+//         return input1 + input2;
+//     } 
+//     else if (operation == "-") {
+//         return input1 - input2;
+//     }
+//     else if (operation == "*") {
+//         return input1 * input2;
+//     }
+//     else if (operation == "/") {
+//         return input1 / input2;
+//     }
+// }
+
+// let result = calculator (input1, input2, operation);
+// console.log (result);
